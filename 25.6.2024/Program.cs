78e7b5b9d97339366dcc14756f21cbb72603b726
Rania <169064319+raniahasi@users.noreply.github.com>
2024-06-26 19:17:12 +0300

    Add files via upload

---
+++ 25.6.2024/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var numbers = new string[5];
+
+            //numbers[0] = Console.ReadLine();
+            //numbers[1] = Console.ReadLine();
+            //numbers[2] = Console.ReadLine();
+            //numbers[3] = Console.ReadLine();
+            //numbers[4] = Console.ReadLine();
+            //for (int i = numbers.Length - 1; i >= 0; i--)
+            //{
+            //    Console.WriteLine(numbers[i]);
+            //}
+
+            //task 1
+            //Console.WriteLine(Console.ReadLine());
+
+            //task 2
+            // a.Double
+            //double doubleVariable = 17.4;
+            //Console.WriteLine(doubleVariable);
+            //// b. String
+            //string stringVariable = "Hosam";
+            //Console.WriteLine(stringVariable);
+            //// c. Char
+            //char charVariable = 'H';
+            //Console.WriteLine(charVariable);
+            //// d. Bool
+            //bool boolVariable = true;
+            //Console.WriteLine(boolVariable);
+            //// e. Int
+            //int intVariable = 555;
+            //Console.WriteLine(intVariable);
+
+            //// f. Const
+            //const double pi = 3.14159;
+            //Console.WriteLine(pi);
+
+            //task 3
+            //string[] car = { "Volvo", "BMW", "Ford", "Mazda" };
+            //Console.WriteLine(car[0]);
+            //Console.WriteLine(car[1]);
+            //Console.WriteLine(car[2]);
+            //Console.WriteLine(car[3]);
+
+            //task 4
+            //string input1 = "Input your firstname: ";
+            //string input2 = "Input your lastname: ";
+            //string input3 = "Input your year of birth: ";
+            //Console.Write(input1);
+            //var firstName = Console.ReadLine();
+            //Console.Write(input2);
+            //var lastName = Console.ReadLine();
+            //Console.Write(input3);
+            //var yearBirth = Console.ReadLine();
+            //Console.WriteLine(firstName + " " + lastName + " " + yearBirth);
+
+            //task 5
+            //var num = new string[10];
+            //Console.Write("element-0:");
+            //num[0] = Console.ReadLine();
+            //Console.Write("element-1:");
+            //num[1] = Console.ReadLine();
+            //Console.Write("element-2:");
+            //num[2] = Console.ReadLine();
+            //Console.Write("element-3:");
+            //num[3] = Console.ReadLine();
+            //Console.Write("element-4:");
+            //num[4] = Console.ReadLine();
+            //Console.Write("element-5:");
+            //num[5] = Console.ReadLine();
+            //Console.Write("element-6:");
+            //num[6] = Console.ReadLine();
+            //Console.Write("element-7:");
+            //num[7] = Console.ReadLine();
+            //Console.Write("element-8:");
+            //num[8] = Console.ReadLine();
+            //Console.Write("element-9:");
+            //num[9] = Console.ReadLine();
+            //for (int i = 0; i < num.Length; i++)
+            //{
+            //    Console.Write(num[i] + " ");
+            //}
+
+
+            //Task 6
+            //var nums = new int[3];
+            //Console.Write("element-0: ");
+            //nums[0] = int.Parse(Console.ReadLine());
+            //Console.Write("element-1: ");
+            //nums[1] = int.Parse(Console.ReadLine());
+            //Console.Write("element-2: ");
+            //nums[2] = int.Parse(Console.ReadLine());
+            //int sumNums = nums[0] + nums[1] + nums[2];
+            //Console.WriteLine("Sum of all elements stored in the array is: " + sumNums);
+
+
+
+
+
+
+
+        }
+    }
+}
